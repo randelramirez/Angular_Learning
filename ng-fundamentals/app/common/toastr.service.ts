2f6656f8c79303b89e320fe260a49f84b862b853
randelramirez <randelramirez1@gmail.com>
2017-09-12 23:16:17 +0800

    test

--- ng-fundamentals/app/common/toastr.service.ts
+++ ng-fundamentals/app/common/toastr.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 declare let toastr: any;
 
 @Injectable()
-export class ToastrServices {
+export class ToastrService {
 
         success(message: string, title?:string){
             toastr.success(message, title);
