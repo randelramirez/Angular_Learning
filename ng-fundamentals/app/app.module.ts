f3f6e1b928d8494f78adb61747cefbcc7d018115
randelramirez <randelramirez1@gmail.com>
2017-09-13 12:45:26 +0800

    barrel implemented for import statements

--- ng-fundamentals/app/app.module.ts
+++ ng-fundamentals/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from "@angular/router";
 import { EventsAppComponent } from './events/events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolverService
+} from "./events/index";
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./common/toastr.service";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { appRoutes } from "./routes";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-import { EventListResolverService } from './events/event-list-resolver.service'
 
 @NgModule({
   imports: [BrowserModule,
@@ -33,12 +35,12 @@ import { EventListResolverService } from './events/event-list-resolver.service'
     ToastrService,
     EventRouteActivator,
     { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState },
-  EventListResolverService],
+    EventListResolverService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
 function checkDirtyState(component: CreateEventComponent) {
-  if(component.isDirty)
+  if (component.isDirty)
     return window.confirm('You have not saved this event, do you really want to cancel?')
   return true;
 }
